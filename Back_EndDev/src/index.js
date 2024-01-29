81ed8d571af761f574a292559a93d047323674d3
Fer2313 <fernandojosefantini7.0@gmail.com>
2024-01-29 20:56:08 -0300

    enviar

--- Back_EndDev/src/index.js
+++ Back_EndDev/src/index.js
@@ -6,9 +6,11 @@ const app = express();
 
 app.use(cors());
 
-app.use(router);
+app.use(express.json())
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(router);
 
 app.listen(3000);
 
