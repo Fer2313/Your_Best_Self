bdba9aaf7ccc97c9332ef9f65033532fc639d37a
Fernando José Fantini <100731742+Fer2313@users.noreply.github.com>
2024-01-30 08:43:45 -0300

    Update db.js

--- Back_EndDev/db.js
+++ Back_EndDev/db.js
@@ -6,12 +6,6 @@ const password = process.env.password;
 const port = process.env.port;
 const database = process.env.database;
 
-const host = process.env.host;
-const user = process.env.user;
-const password = process.env.password;
-const port = process.env.port;
-const database = process.env.database;
-
 export const pool = createPool({
     host:`${host}`,
     user:`${user}`,
